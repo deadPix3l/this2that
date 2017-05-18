26bde9129942332d08515cd71ecc9bf6ec8a1cda
deadpix3l <skylerr.curtis@gmail.com>
2017-05-18 17:59:36 +0000

    better code style and supported indentation

--- out/xml.cpp
+++ out/xml.cpp
@@ -4,30 +4,49 @@
 
 //xml globals
 int indent = 1;
+int tablen = 4;
 std::stack<char*> mystack;
 
+//helper functions
+//------------------
+void printIndent(int indent) {
+    printf("%*s", indent*tablen, "");
+}
+
+// need to write an escapeKey() function.
+// <the key> is invalid; it's a <the> element with an incomplete 'key' attr
+// needs to be turned into <the-key>
 
 //xml functions
 //-----------------
-extern "C" void AssignStr(char* key, char* value) 
-{ printf("%*s<%s>%s</%s>\n", (indent)*4, "", key, value, key); }
+extern "C" void AssignStr(char* key, char* value) {
+    printIndent(indent);
+    printf("<%s>%s</%s>\n", key, value, key);
+}
 
-extern "C" void AssignQuoted(char* key, char* value)
-{ printf("%*s<%s>%s\b</%s>\n", (indent)*4, "", key, value+1, key); }
+//extern "C" void AssignQuoted(char* key, char* value)
+//{ printf("%*s<%s>%s\b</%s>\n", (indent)*4, "", key, value+1, key); }
 
-extern "C" void AssignFloat(char* key, double value) 
-{ printf("%*s<%s>%.2f</%s>", (indent)*4, "", key, value, key); }
+extern "C" void AssignFloat(char* key, double value) {
+    printIndent(indent);
+    printf("<%s>%f</%s>\n", key, value, key);
+}
 
-extern "C" void AssignInt(char* key, int value) 
-{ printf("%*s<%s>%d</%s>\n", (indent)*4, "", key, value, key); }
+extern "C" void AssignInt(char* key, int value) {
+    printIndent(indent);
+    printf("<%s>%d</%s>\n", key, value, key);
+}
 
-extern "C" void StartObj(char* key) { 
-    printf("%*s<%s>\n", (indent++)*4, "", key); 
+extern "C" void StartObj(char* key) {
+    printf("\n");
+    printIndent(indent++);
+    printf("<%s>\n", key);
     mystack.push(key);
 }
 
-extern "C" void EndObj(char* key) { 
-    printf("%*s</%s>\n\n", (--indent)*4, "", mystack.top()); 
+extern "C" void EndObj(char* key) {
+    printIndent(--indent);
+    printf("</%s>\n", mystack.top());
     mystack.pop();
 }
 
