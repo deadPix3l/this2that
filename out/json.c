e9727e8ce6a70eb37858f0bcf4c7f6b689b2c8e2
deadpix3l <skylerr.curtis@gmail.com>
2017-05-18 17:55:24 +0000

    fixed comma issue. no more backspaces

--- out/json.c
+++ out/json.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
+//globals
+int RS = 0;
+
 //json functions
 //-----------------
-void AssignStr(char* key, char* value) 
-{ printf("\n\"%s\": \"%s\",", key, value); }
+void AssignStr(char* key, char* value) {
+    if(RS) printf(",");
+    printf("\n\"%s\": \"%s\"", key, value);
+    RS = 1;
+}
 
-void AssignQuoted(char* key, char* value)
-{ printf("\n\"%s\": %s,", key, value); }
+//void AssignQuoted(char* key, char* value)
+//{ printf("\n\"%s\": \"%s\",", key, value); }
 
-void AssignFloat(char* key, double value) 
-{ printf("\n\"%s\": %f,", key, value); }
+void AssignFloat(char* key, double value) {
+    if(RS) printf(",");
+    printf("\n\"%s\": %f", key, value);
+    RS = 1;
+}
 
-void AssignInt(char* key, int value) 
-{ printf("\n\"%s\": %d,", key, value); }
+void AssignInt(char* key, int value) {
+    if(RS) printf(",");
+    printf("\n\"%s\": %d", key, value);
+    RS = 1;
+}
 
+void StartObj(char* key) {
+    if(RS) printf(",");
+    printf("\n\n\"%s\": {", key);
+    RS = 0;
+}
 
-void StartObj(char* key) 
-{ printf("\n\n\"%s\": { ", key); }
+void EndObj(char* key) {
+    printf("\n}");
+    RS = 1;
+}
 
-void EndObj(char* key) 
-{ printf("\b \n},"); }
-
-void prefix() { printf("{ \n"); }
-void postfix() { printf("\b}\n"); }
+void prefix() { printf("{"); }
+void postfix() { printf("\n}"); }
 
 //void flush();
